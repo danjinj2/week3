eaad0fa339ecea821928f63b21df6a326767c532
Jiang Danjin <jiangdanjin@wirelessprv-10-194-151-201.near.illinois.edu>
2018-09-19 20:24:32 -0500

    assignment

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function Multiple(array) {
 }
 let array = [1,2,3,4,5]
 console.log(Multiple(array))
+
+function concatenation(strings) {
+  let concatenate = strings.reduce((prev, elem) => prev + elem)
+  return concatenate
+}
+
+let strings = ["My"," name"," is"," Bevy"]
+console.log(concatenation(strings))
